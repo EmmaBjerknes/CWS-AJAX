f8216374ba5d43fd00a68cfae90e7309787f5eae
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-01-18 14:14:09 +0100

    Seed code + a try at async, await

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+const countryDiv:HTMLDivElement = document.querySelector('.display-country') as HTMLDivElement;
+
+const countryName:HTMLInputElement = document.querySelector('.countryNameInput') as HTMLInputElement;
+
+const searchName:HTMLButtonElement = document.querySelector('#countryName') as HTMLButtonElement;
+
+let url: string = "https://restcountries.com/v3.1/";
+
+let countryArray:object[] = [];
+
+
+searchName.addEventListener('click', function(event){
+    event.preventDefault();
+    let valueOfInput = countryName.value;
+    url += "name/" + valueOfInput; 
+    console.log(url)
+    foo(url);
+    
+});
+
+async function foo(x:string) {
+    const response = await fetch(x);
+    const data = await response.json();
+    countryArray = await data;
+    myCountry(countryArray);
+}
+
+function myCountry(pickedCountry:any){
+    console.log(typeof pickedCountry);
+    console.log(pickedCountry[0].name.common);
+}
